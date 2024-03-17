30181cea7ad89da0a31956a3653c4f3cf67b0648
tusharkantitimaji <tushar.maji@pagarbook.com>
2024-03-17 15:03:45 +0530

    UI Page addition

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
+  // let queryOptions = { active: true, lastFocusedWindow: true };
+  // let [tab] = await chrome.tabs.query(queryOptions);
 
-  const currentUrl = tab.url;
+  // const currentUrl = tab.url;
 
-  const urlObject = new URL(currentUrl);
-  const urlBeforeSearchParam = urlObject.origin + urlObject.pathname;
+  // const urlObject = new URL(currentUrl);
+  // const urlBeforeSearchParam = urlObject.origin + urlObject.pathname;
 
-  const currentTabUrlElement = document.getElementById('currentTabUrl');
-  currentTabUrlElement.innerHTML = urlBeforeSearchParam;
+  // const currentTabUrlElement = document.getElementById('currentTabUrl');
+  // currentTabUrlElement.innerHTML = urlBeforeSearchParam;
 
+  const page0Element = document.getElementById('page0');
   const page1Element = document.getElementById('page1');
   const page2Element = document.getElementById('page2');
   const page3Element = document.getElementById('page3');
   const page4Element = document.getElementById('page4');
 
-  document.getElementById('confirmBtn').addEventListener('click', function() {
+  document.getElementById('setPasswordBtn').addEventListener('click', function() {
     const password = prompt('Enter password:');
     if (password !== null) {
-      const showPasswordElement = document.getElementById('passwordContent');
-
-      showPasswordElement.innerHTML = ` ${password}`;
-      page1Element.style.display = 'none';
-      page2Element.style.display = 'block';
+      page0Element.style.display = 'none';
+      page1Element.style.display = 'block';
     }
   });
 
+  document.getElementById('goHomeBtn').addEventListener('click', function() {
+    console.log("goHomeBtn clicked")
+    page1Element.style.display = 'none';
+    page3Element.style.display = 'none';
+    page4Element.style.display = 'none';
+    page2Element.style.display = 'block';
+  });
 
   document.getElementById('lockBtn').addEventListener('click', async function() {
-    const successStoring = await storePassword(currentUrl, password);
+    console.log("lockBto clicked")
+    const successStoring = true; // await storePassword(currentUrl, password);
 
     if (successStoring) {
       page3Element.style.display = 'block';
