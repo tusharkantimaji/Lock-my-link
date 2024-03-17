2ecf1d7554663272803a145e58a2e48b8be4cec9
tusharkantitimaji <tushar.maji@pagarbook.com>
2024-03-17 18:03:57 +0530

    Lock and Unlock button text handled

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  // let queryOptions = { active: true, lastFocusedWindow: true };
-  // let [tab] = await chrome.tabs.query(queryOptions);
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
 
-  // const currentUrl = tab.url;
+  const currentUrl = tab.url;
 
-  // const urlObject = new URL(currentUrl);
-  // const urlBeforeSearchParam = urlObject.origin + urlObject.pathname;
+  const urlObject = new URL(currentUrl);
+  const urlBeforeSearchParam = urlObject.origin + urlObject.pathname;
 
-  // const currentTabUrlElement = document.getElementById('currentTabUrl');
-  // currentTabUrlElement.innerHTML = urlBeforeSearchParam;
+  const currentTabUrlElement = document.getElementById('currentTabUrl');
+  currentTabUrlElement.innerHTML = urlBeforeSearchParam;
+  let isCurrentUrlLocked = false; // TODO: await isUrlLocked(currentUrl);
 
   const page0Element = document.getElementById('page0');
   const page1Element = document.getElementById('page1');
   const page2Element = document.getElementById('page2');
   const page3Element = document.getElementById('page3');
   const page4Element = document.getElementById('page4');
+  const lockOrUnlockBtnElement = document.getElementById('lockOrUnlockBtn');
+  const areYouSureElement = document.getElementById('areYouSure');
+
+  updateLockAndUnlockButton(isCurrentUrlLocked, lockOrUnlockBtnElement, areYouSureElement);
 
   document.getElementById('setPasswordBtn').addEventListener('click', function() {
     const password = prompt('Enter password:');
@@ -27,7 +32,6 @@ document.addEventListener("DOMContentLoaded", async () => {
   const goHomeButtons = document.querySelectorAll('.goHomeBtn');
   goHomeButtons.forEach(button => {
     button.addEventListener('click', () => {
-      console.log("goHomeBtn clicked")
       page1Element.style.display = 'none';
       page3Element.style.display = 'none';
       page4Element.style.display = 'none';
@@ -35,12 +39,22 @@ document.addEventListener("DOMContentLoaded", async () => {
     });
   });
 
-  document.getElementById('lockBtn').addEventListener('click', async function() {
-    console.log("lockBto clicked")
-    const successStoring = false; // await storePassword(currentUrl, password);
+  // TODO:
+  document.getElementById('lockOrUnlockBtn').addEventListener('click', async function() {
+    let successfulUpdate = true; // await storePassword(currentUrl, password);
+    if (isCurrentUrlLocked) {
+      // await unlockUrl(currentUrl);
+      successfulUpdate = true;
+    }
+    else {
+      // await lockUrl(currentUrl);
+      successfulUpdate = true;
+    }
 
-    if (successStoring) {
+    if (successfulUpdate) {
       page3Element.style.display = 'block';
+      isCurrentUrlLocked = !isCurrentUrlLocked;
+      updateLockAndUnlockButton(isCurrentUrlLocked, lockOrUnlockBtnElement, areYouSureElement);
     }
     else {
       page4Element.style.display = 'block';
@@ -55,4 +69,29 @@ async function storePassword(url, password) {
   console.log(url, password);
 
   return false;
+}
+
+function updateLockAndUnlockButton(isCurrentUrlLocked, lockOrUnlockBtnElement, areYouSureElement) {
+  let homePageInfo = {
+    buttonText: String,
+    buttonColor: String,
+    descriptionMessage: String
+  }
+
+  if (isCurrentUrlLocked) {
+    homePageInfo = {
+      buttonText: 'Unlock',
+      buttonColor: 'green',
+      descriptionMessage: 'To unlock this URL, Click the Unlock Button'
+    }
+  } else {
+    homePageInfo = {
+      buttonText: 'Lock',
+      buttonColor: 'red',
+      descriptionMessage: 'To lock this URL, Click the Lock Button'
+    }
+  }
+  lockOrUnlockBtnElement.innerHTML = homePageInfo.buttonText;
+  lockOrUnlockBtnElement.style.backgroundColor = homePageInfo.buttonColor;
+  areYouSureElement.innerHTML = homePageInfo.descriptionMessage;
 }
